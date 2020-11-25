9613a197da92351e6066e1fe67312c282c85cac6
nonameswereavailable <61844399+nonameswereavailable@users.noreply.github.com>
2020-11-25 02:55:43 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+def sayhi():
+    def sayhi1():
+        def sayhi2():
+            def sayhi3():
+                def sayhi4():
+                    def sayhi5():
+                        def sayhi6():
+                            def sayhi7():
+                                def sayhi8():
+                                    def sayhi9():
+                                        def sayhi10():
+                                            def sayhi11():
+                                                def sayhi12():
+                                                    def sayhi13():
+                                                        def sayhi14():
+                                                            def sayhi15():
+                                                                print("Hello World")
+                                                            return sayhi15
+                                                        return sayhi14
+                                                    return sayhi13
+                                                return sayhi12
+                                            return sayhi11
+                                        return sayhi10
+                                    return sayhi9
+                                return sayhi8
+                            return sayhi7
+                        return sayhi6
+                    return sayhi5
+                return sayhi4
+            return sayhi3
+        return sayhi2
+    return sayhi1
+
+
+
+sayhi()()()()()()()()()()()()()()()()
